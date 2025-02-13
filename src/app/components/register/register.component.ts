67e3e62eba523085ff9713808200c17e4cdf2166
Mehdi-Elargoubi <mehdi.elargoubi11@gmail.com>
2025-02-13 23:24:48 +0100

    Update Register form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,6 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { FormControl, Validators } from '@angular/forms';
+import { merge } from 'rxjs';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-register',
@@ -8,20 +11,45 @@ import { Router } from '@angular/router';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  email: string = '';
-  password: string = '';
   private authService = inject(AuthService);
   private router = inject(Router);
 
-  constructor() {}
+  email = new FormControl('', [Validators.required, Validators.email]);
+  //email: string = '';
+  password: string = '';
+  hide = signal(true);
+  errorMessage = signal('');
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+    .pipe(takeUntilDestroyed())
+    .subscribe(() => this.updateErrorMessage());
+  }
 
   async register() {
     try {
-      await this.authService.register(this.email, this.password);
+      await this.authService.register(this.email.value! , this.password);
       this.router.navigate(['/dashboard']);
     } catch (error) {
       console.error(error);
     }
+  }
+
+
+  updateErrorMessage(): void {
+    if (this.email.hasError('required')) {
+      this.errorMessage.set('You must enter a value');
+    } else if (this.email.hasError('email')) {
+      this.errorMessage.set('Not a valid email');
+    } else {
+      this.errorMessage.set('');
+    }
+  }
+
+  toggleHide(event: MouseEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.hide.set(!this.hide());
   }
 
 }
