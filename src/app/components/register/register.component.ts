b711fa741c5df891f1371bb7fa69fc834d23b92c
Hanae Echared <echaredhanae43@gmail.com>
2025-03-28 00:14:50 +0100

    Dérnière mise à jour

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 import { merge } from 'rxjs';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { createUserWithEmailAndPassword, sendEmailVerification, Auth } from '@angular/fire/auth';
@@ -13,66 +13,50 @@ import { MatSnackBar } from '@angular/material/snack-bar';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-  private authService = inject(AuthService);
   private router = inject(Router);
   private auth = inject(Auth);
   private snackBar = inject(MatSnackBar);
 
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password: string = '';
-  hide = signal(true);
-  errorMessage = signal('');
-  verificationMessage = signal('');
+  registerForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+\\-={}\\[\\]|;:"<>,.?/~`]).{8,}$')
+    ]),
+    confirmPassword: new FormControl('', Validators.required),
+  });
 
-  constructor() {
-    merge(this.email.statusChanges, this.email.valueChanges)
-    .pipe(takeUntilDestroyed())
-    .subscribe(() => this.updateErrorMessage());
-  }
+  errorMessage = signal('');
 
   register(): void {
-    createUserWithEmailAndPassword(this.auth, this.email.value!, this.password)
+    if (this.registerForm.invalid || this.password?.value !== this.confirmPassword?.value) {
+      this.errorMessage.set("Les mots de passe ne correspondent pas ou champs invalides.");
+      return;
+    }
+
+    createUserWithEmailAndPassword(this.auth, this.email?.value!, this.password?.value!)
       .then((userCredential) => {
-        sendEmailVerification(userCredential.user)
-          .then(() => {
-            console.log('Email de vérification envoyé');
-            this.snackBar.open('Un email de vérification a été envoyé - vérifier votre boîte de réception mail', 'Fermer', {
-              duration: 5000,
-              verticalPosition: 'top',
-            });
-            this.auth.signOut();
-          })
-          .catch((error) => {
-            console.error('Erreur d envoi de mail de vérification :', error);
-            this.snackBar.open('Erreur d envoi de mail de vérification - vérifier @ email', 'Fermer', {
-              duration: 5000,
-              verticalPosition: 'top',
-            });
+        sendEmailVerification(userCredential.user).then(() => {
+          this.snackBar.open('Email de vérification envoyé.', 'Fermer', {
+            duration: 5000,
+            verticalPosition: 'top',
           });
+          this.auth.signOut();
+        });
       })
       .catch((error) => {
-        console.error('Erreur d inscription:', error);
-        this.snackBar.open('Erreur d inscription - vérifier vos infos', 'Fermer', {
+        this.snackBar.open("Erreur d'inscription", 'Fermer', {
           duration: 5000,
           verticalPosition: 'top',
         });
       });
   }
 
-  updateErrorMessage(): void {
-    if (this.email.hasError('required')) {
-      this.errorMessage.set('You must enter a value');
-    } else if (this.email.hasError('email')) {
-      this.errorMessage.set('Not a valid email');
-    } else {
-      this.errorMessage.set('');
-    }
-  }
-
-  toggleHide(event: MouseEvent): void {
-    event.preventDefault();
-    event.stopPropagation();
-    this.hide.set(!this.hide());
-  }
-
+  get firstName() { return this.registerForm.get('firstName'); }
+  get lastName() { return this.registerForm.get('lastName'); }
+  get email() { return this.registerForm.get('email'); }
+  get password() { return this.registerForm.get('password'); }
+  get confirmPassword() { return this.registerForm.get('confirmPassword'); }
 }
